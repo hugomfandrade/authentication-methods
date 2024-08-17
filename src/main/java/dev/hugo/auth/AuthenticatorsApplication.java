49bebfef982835ae0e6f27097ef642422bf0b767
Hugo Andrade <hugoa@nuada.pt>
2024-08-17 15:04:38 +0100

    initial commit

---
+++ src/main/java/dev/hugo/auth/AuthenticatorsApplication.java
@@ -0,0 +1,13 @@
+package dev.hugo.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hugo/auth/AuthenticatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hugo.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
